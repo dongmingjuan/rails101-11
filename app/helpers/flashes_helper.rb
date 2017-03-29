c31953893e5fe040de38ee80b80d55212022be5b
dongmingjuan <d387918156@gmail.com>
2017-03-29 14:03:05 +0800

    create groups index

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,4 +1,4 @@
-Module FlashesHelper
+module FlashesHelper
   FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning" }.freeze
 
   def flash_class (key)
@@ -6,6 +6,6 @@ Module FlashesHelper
   end
 
   def user_facing_flashes
-    flash.to_hash.silice "alert", "notice", "warning"
+    flash.to_hash.slice "alert", "notice", "warning"
   end
 end
